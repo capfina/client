645f78592e2370b63b3ab2672cadb541d5670c78
RepoContributor <repo-contributor@users.noreply.github.com>
2021-06-14 12:33:00 +0100

    standard update

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -11,16 +11,12 @@ export const NETWORKS = {
 		L1_CHAIN_ID: '0x4',
 		NETWORK_RPC_URL: 'https://rinkeby.arbitrum.io/rpc',
 		EXPLORER_URL: 'https://rinkeby-explorer.arbitrum.io',
-		L1_BLOCK_DURATION: 15,
-		ARB_RETRYABLE_TX_ADDRESS: '0x000000000000000000000000000000000000006E'.toLowerCase(),
-		ARB_NODE_INTERFACE_ADDRESS: '0x00000000000000000000000000000000000000C8'.toLowerCase()
+		L1_BLOCK_DURATION: 15
 	},
 	'0x4': { // Rinkeby Testnet
 		L2_CHAIN_ID: '0x66eeb',
 		NETWORK_RPC_URL: 'https://rinkeby.infura.io/v3/0bb669b7a5c3417bbf521b6b1c321991',
-		EXPLORER_URL: 'https://rinkeby.etherscan.io/',
-		ARBITRUM_L1_GATEWAY_ROUTER_ADDRESS: '0x70c143928ecffaf9f5b406f7f4fc28dc43d68380'.toLowerCase(),
-		ARBITRUM_INBOX_ADDRESS: '0x578BAde599406A8fE3d24Fd7f7211c0911F5B29e'.toLowerCase(),
+		EXPLORER_URL: 'https://rinkeby.etherscan.io/'
 	}
 }
 
